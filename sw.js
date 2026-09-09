a75492ee4446c364741968de593a2ed33b5757cc
abu00salman <45781023+abu00salman@users.noreply.github.com>
2026-09-09 07:11:38 +0300

    Add files via upload
    nabda app

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE = 'nabda-v1';
+const FILES = ['./', './index.html', './manifest.json', './icon.svg'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET' || !e.request.url.startsWith(self.location.origin)) return;
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => { const copy = res.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)); return res; })));
+});
