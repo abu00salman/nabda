1972306ebfdd05744c61c6da69c43faf94b10d92
abu00salman <45781023+abu00salman@users.noreply.github.com>
2026-09-10 07:42:25 +0300

    Add files via upload
    اعلانات للعبة

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nabda-v4';
+const CACHE = 'nabda-v5';
 const FILES = ['./', './index.html', './manifest.json', './icon.svg', './og-image.jpg', './about.html', './privacy.html'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
